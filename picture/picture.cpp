f516c16c5f7648449ea95e66827a038e70560f50
jialiujia <jiayilun@jiayilundeMacBook-Pro.local>
2018-06-18 23:38:24 +0800

    添加图的邻接表和邻接矩阵定义
    添加图的邻接表和邻接矩阵定义

---
+++ picture/picture.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 贾屹伦 on 2018/6/18.
+//
+
+#include "picture.hpp"
---
+++ picture/picture.hpp
@@ -0,0 +1,43 @@
+//
+// Created by 贾屹伦 on 2018/6/18.
+//
+
+#ifndef DATA_STRUCT2019_PICTURE_HPP
+#define DATA_STRUCT2019_PICTURE_HPP
+
+#define MAXSIZE 100
+
+///领接矩阵
+typedef struct {
+    int no; //顶点编号
+    char data;
+} VertexType;
+
+///领接矩阵图
+typedef struct {
+    float edges[MAXSIZE][MAXSIZE];
+    int n, e; //顶点数和边数
+    VertexType vex[MAXSIZE];
+} MGraph;
+
+///邻接表边
+typedef struct arc_node {
+    int adjvex; //该边所指向的结点
+    arc_node *nextArc; //指向下一条边
+    float info; //边的相关信息，如权值
+} ArcNode;
+
+///邻接表结点
+typedef struct {
+    char data;
+    arc_node *firstArc; //指向第一条边
+}VNode;
+
+///邻接表图
+typedef struct {
+    VNode adjlist[MAXSIZE];
+    int n, e; //顶点数和边数
+} AGraph;
+
+
+#endif //DATA_STRUCT2019_PICTURE_HPP
